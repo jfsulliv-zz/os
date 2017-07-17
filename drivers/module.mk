ee6839b7fc56ff0c8e0f2b276952cbc6ad389fc1
JamesSullivan1 <sullivan.james.f@gmail.com>
2017-07-16 20:10:24 -0400

    Remove unimplemented driver modules

--- drivers/module.mk
+++ drivers/module.mk
@@ -1,15 +1,8 @@
 MODULES += $(dir)
 
-sp := $(sp).x
-dirstack_$(sp) := $(d)
-d  := $(dir)
+sp              := $(sp).x
+dirstack_$(sp)  := $(d)
+d               := $(dir)
 
-
-dir := $(d)/acpi
-include $(dir)/module.mk
-dir := $(d)/ahci
-include $(dir)/module.mk
-
-
-d := $(dirstack_$(sp))
-sp := $(basename $(sp))
+d               := $(dirstack_$(sp))
+sp              := $(basename $(sp))
